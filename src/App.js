3ad6eeae94c834608924ac7afcfa437e59131003
Kundan Challa <59391210+Kundan2406@users.noreply.github.com>
2024-08-22 16:09:20 +0530

    app js changes routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import './css/stylesheet.css';
 
@@ -15,46 +15,31 @@ import LoginSuccess from './Project/Login/loginSuccess';
 import RegisterSuccess from './Project/Register/registerSuccess';
 import NotFound from './Project/Welcome/notFound';
 
+
+const PrivateRoute = ({ children }) => {
+    const isLoggedIn = !!localStorage.getItem('LoggedInfo'); // Check if user is logged in
+    return isLoggedIn ? children : <Navigate to="" />;
+};
+
 const App = () => {
-  const [loginInfo, setLoginInfo] = useState(() => JSON.parse(localStorage.getItem('LoggedInfo')) || {});
-  const isLoggedIn = Boolean(loginInfo && Object.keys(loginInfo).length);
-
-  // Update the state when localStorage changes
-  useEffect(() => {
-    const handleStorageChange = () => {
-      setLoginInfo(JSON.parse(localStorage.getItem('LoggedInfo')) || {});
-    };
-
-    window.addEventListener('storage', handleStorageChange);
-
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, []);
-
-  // Optionally, directly update the state when handling login/logout
-  const handleLogin = (newLoginInfo) => {
-    localStorage.setItem('LoggedInfo', JSON.stringify(newLoginInfo));
-    setLoginInfo(newLoginInfo);
-  };
-
+  
   return (
     <BrowserRouter>
       <Routes>
         <Route path="" element={<Menu />}>
-          <Route path="/" element={isLoggedIn ? <UserList /> : <Navigate to="/welcome" />} />
-          <Route path="chats" element={isLoggedIn ? <Chats /> : <Navigate to="/welcome" />} />
-          <Route path="users" element={isLoggedIn ? <UserList /> : <Navigate to="/welcome" />} />
-          <Route path="users/edit-user" element={isLoggedIn ? <EditUser /> : <Navigate to="/welcome" />} />
-          <Route path="document" element={isLoggedIn ? <Document /> : <Navigate to="/welcome" />} />
+          <Route path="" element={ <PrivateRoute><UserList /></PrivateRoute> } />
+          <Route path="chats" element={ <PrivateRoute><Chats /></PrivateRoute> } />
+          <Route path="users" element={ <PrivateRoute><UserList /></PrivateRoute> } />
+          <Route path="users/edit-user" element={ <PrivateRoute><EditUser /></PrivateRoute> } />
+          <Route path="document" element={ <PrivateRoute><Document /></PrivateRoute> } />
           <Route path="loginsuccess" element={<LoginSuccess />} />
         </Route>
 
         <Route path="logout" element={<Logout />} />
-        <Route path="login" element={!isLoggedIn ? <Login onLogin={handleLogin} /> : <Navigate to="/users" replace />} />
-        <Route path="welcome" element={!isLoggedIn ? <Welcome /> : <Navigate to="/users" replace />} />
-        <Route path="register" element={!isLoggedIn ? <Register /> : <Navigate to="/users" replace />} />
-        <Route path="registersuccess" element={!isLoggedIn ? <RegisterSuccess /> : <Navigate to="/users" replace />} />
+        <Route path="login" element={ <Login /> } />
+        <Route path="welcome" element={ <Welcome /> } />
+        <Route path="register" element={ <Register /> } />
+        <Route path="registersuccess" element={ <RegisterSuccess /> } />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
